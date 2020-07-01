4bd03a752e050bdc1fb5a5676591313ad09d9c62
Srikanth <srikanthamudala95@gmail.com>
2020-06-30 22:43:48 -0700

    Postgres SQL Integration, super user created.

--- djangoProjectV1/djangoProjectV1/settings.py
+++ djangoProjectV1/djangoProjectV1/settings.py
@@ -84,10 +84,20 @@ WSGI_APPLICATION = 'djangoProjectV1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
